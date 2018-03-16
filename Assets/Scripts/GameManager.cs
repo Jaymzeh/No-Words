0260920e8270e8bc9765aa0d6b614437a829193a
James Smith <james_smith.games@live.co.uk>
2018-03-16 21:47:22 +0000

    Implemented Silhouette puzzle prototype.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,23 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager Instance;
 
-
-
-    public static void StartGame() {
-
+    [SerializeField]
+    public WordSequence[] sequences;
+    public int sequenceGroupIndex = 0;
+    public int sequenceIndex;
+    
+    void Start() {
+        FindObjectOfType<Silhoutte>().targetRotation = sequences[sequenceGroupIndex].targetRotation;
     }
+
+}
+
+[System.Serializable]
+public class WordSequence {
+    public string[] words;
+
+    public Sprite[] checkerImages;
+    public Sprite[] crowdImages;
+    public GameObject mesh;
+    public Vector3 targetRotation;
 }
--- Assets/Scripts/Puzzles/CrowdControl.cs
+++ Assets/Scripts/Puzzles/CrowdControl.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrowdControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform player;
+    public Transform[] group;
+    public Transform[] wheel;
+    public int[] range;
+    public int threshold = 10;
+    public float rotationSpeed = 1;
+
+    void Update() {
+        for (int i = 0; i < 2; i++) {
+            if (Vector3.Distance(player.position, wheel[i].position) <= 2) {
+                if (Input.GetAxis("Submit") >= 0)
+                    wheel[i].Rotate(0, 0, rotationSpeed);
+            }
+        }
+    }
+
+    bool InRange(int index) {
+        if (wheel[index].eulerAngles.z >= range[index] - threshold && wheel[index].eulerAngles.z <= range[index] + threshold)
+            return true;
+        return false;
+    }
 }
