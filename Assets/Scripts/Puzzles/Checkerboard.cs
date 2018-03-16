d692999696c3595603f877b65bd068b70c47f7e9
James Smith <james_smith.games@live.co.uk>
2018-03-16 15:55:53 +0000

    Project setup. First prototype of Checkerboard puzzle implemented.

---
+++ Assets/Scripts/Puzzles/Checkerboard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Checkerboard : MonoBehaviour {
+
+    public string Word { get { return word; } private set { word = value; } }
+    string word;
+
+    public float moveMultiply = 1;
+
+    public Image[] images;
+    BoxCollider2D collider;
+    Transform colliderObject;
+
+
+    void Start() {
+        if (images == null) {
+            Debug.LogError("Puzzle Image components not Assigned");
+            return;
+        }
+
+
+        collider = images[0].GetComponentInChildren<BoxCollider2D>();
+        colliderObject = images[1].transform.GetChild(0);
+    }
+
+
+    void Update() {
+
+        images[0].transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+
+
+        if (Input.GetAxis("Submit") > 0) {
+            if (CanSolve()) {
+
+                images[0].transform.position = colliderObject.transform.position;
+
+                Debug.Log("SOLVED!");
+            }
+            else
+                Debug.Log("NOT solved");
+        }
+    }
+
+    bool CanSolve() {
+
+        if (collider.bounds.Contains(colliderObject.position))
+            return true;
+
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/UI/GeneralUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralUI : MonoBehaviour {
+
+	public static void ChangeScene(string scene) {
+
+    }
+
+    public static string GetScene() {
+        return null;
+    }
+
+
+}
